6fa2d7029b36a4735c45390dbf791e3059f6fae0
DESKTOP-JOSMGD6\HY <made1999@naver.com>
2024-03-12 21:17:21 +0900

    이하영 #7 #8 - 메모 화면 구성, 메모 저장  API 기능 구현 - 리스트 화면 구성, 해당 상세 메모 조회 기능 구현, session 객체 사용해서 로그인한 사용자의 메모 리스트 조회

---
+++ src/main/java/com/ajfqo/memo/post/service/PostService.java
@@ -0,0 +1,41 @@
+package com.ajfqo.memo.post.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ajfqo.memo.post.domain.Post;
+import com.ajfqo.memo.post.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post addPost(int userId, String title, String contents) {
+		
+		Post post = Post.builder()
+					.userId(userId)
+					.title(title)
+					.contents(contents)
+					.build();
+		
+		return postRepository.save(post);
+	}
+	
+	// 특정 사용자의 메모 목록을 돌려준다
+	public List<Post> getPostList(int userId) {
+		return postRepository.findByUserIdOrderByIdDesc(userId);
+	}
+	
+	public Post getPost(int id) {
+		Optional<Post> optionalPost =  postRepository.findById(id);
+		Post post = optionalPost.orElse(null);
+		return post;
+	}
+	
+	
+}
