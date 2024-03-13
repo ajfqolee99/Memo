0c72952a1e220a6d4d72677e9a2e8f93aeba8337
DESKTOP-JOSMGD6\HY <made1999@naver.com>
2024-03-13 21:57:41 +0900

    이하영 #9 - MultipartFile 객체사용해서 저장할 폴더 경로, 파일 경로, 파일 저장 기능 추가 - WebMvcConfigurer 메소드 사용해서 설정한 파일로 경로 이동 기능 추가

---
+++ src/main/java/com/ajfqo/memo/common/FileManager.java
@@ -0,0 +1,61 @@
+package com.ajfqo.memo.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileManager {
+	
+	// 상수
+	public final static String FILE_UPLOAD_PATH = "D:\\01.STUDY\\springProject\\upload\\memo";
+	
+	public static String saveFile(int userId, MultipartFile file) {
+		
+		// 같은 이름의 파일 처리
+		// 폴더(디렉토리) 만들어서 파일 저장
+		// 로그인 사용자의 userId를 폴더 이름으로 지정
+		// 현재시간 정보를 폴더 이름에 포함
+		// UNIX TIME : 1970년 1월 1일부터 흐른시간을 milli second (1/1000)로 표현한 시간
+		// ex) 2_109238419
+		
+		// 폴더 이름 만들기
+		String directoryName="/" + userId + "_" + System.currentTimeMillis();
+		
+		// 폴더 생성
+		String directoryPath = FILE_UPLOAD_PATH + directoryName;
+		File directory = new File(directoryPath);
+		
+		if(!directory.mkdir()) {
+			//디렉토리 생성 실패
+			return null;
+		}
+		
+		// 파일 저장
+		String filePath = directoryPath + "/" + file.getOriginalFilename();
+		
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(filePath);
+			Files.write(path, bytes);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			// 파일 저장 실패
+			return null;
+		}
+		
+		// 클라이언트에서 접근가능한 경로 - 데이터베이스에 저장
+		// 클라이언트에서 접근가능한 경로 리턴
+		
+		// 파일 경로 : D:\\01.STUDY\\springProject\\upload\\memo/2_109238419/test.png
+		// url 경로 규칙 : /images/2_109238419/test.png
+		
+		return "/images" + directoryName + "/" + file.getOriginalFilename();
+		
+	}
+	
+}
